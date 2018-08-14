0fbb6df92e5a75c19d82a6ea52ee2ebc638c433d
Elena_Mih <8.Lisi4ka.8@gmail.com>
2018-08-14 13:09:37 +0300

    testing

---
+++ Jetro/js/main.js
@@ -0,0 +1,18 @@
+ $('.js-slider-for').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      asNavFor: '.js-slider-nav',
+      autoplay: true
+    });
+    $('.js-slider-nav').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      asNavFor: '.js-slider-for',
+      arrows: false,
+      focusOnSelect: true
+    });
+
+$('.slider-blog').slick({
+    autoplay: true,
+    autoplaySpeed: 5000
+});
